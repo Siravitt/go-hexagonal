3283e8e4424e375d7af512a3e6c3ea3046184368
A667450 A667450 <a667450@AR8040667450N1.local>
2023-12-16 23:53:34 +0700

    :art: improve structure

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/Siravitt/go-hexagonal/handler"
 	"github.com/Siravitt/go-hexagonal/repository"
@@ -16,17 +18,9 @@ import (
 
 func main() {
 	initConfig()
-	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
-		viper.GetString("db.username"),
-		viper.GetString("db.password"),
-		viper.GetString("db.host"),
-		viper.GetInt("db.port"),
-		viper.GetString("db.database"),
-	)
-	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
-	if err != nil {
-		panic(err)
-	}
+	initTimeZone()
+
+	db := initDatabase()
 
 	userRepositoryDB := repository.NewUserRepositoryDB(db)
 	userRepositoryMock := repository.NewUserRepositoryMock()
@@ -53,9 +47,39 @@ func initConfig() {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db
 }
